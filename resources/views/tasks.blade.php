0901b676204731a253c2b532b97c3c7944047644
3A632009 <3a632009@student.ncut.edu.tw>
2019-10-23 11:12:02 +0800

    刪除任務

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -67,10 +67,8 @@
                                 <form action="/task/{{ $task->id }}" method="POST">
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
-
                                     <button>刪除任務</button>
                                 </form>
-
 
                             </td>
                         </tr>
